68f5cf861756b1e8bd768953e6a9eef2175434da
Austin Godber <godber@uberhip.com>
2025-10-02 07:16:50 -0700

    embed fontawesome

--- src/embeddingbuddy/app.py
+++ src/embeddingbuddy/app.py
@@ -25,7 +25,6 @@ def create_app():
         title="EmbeddingBuddy",
         external_stylesheets=[
             dbc.themes.BOOTSTRAP,
-            "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
         ],
         assets_folder=assets_path,
         meta_tags=[
